2b043378a832ee8d57ba1a224eae1813f150a642
Chunhui He <hchunhui@mail.ustc.edu.cn>
2018-03-31 11:26:08 +0000

    add ota update

--- main/simple_wifi.c
+++ main/simple_wifi.c
@@ -14,12 +14,15 @@
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
 #include "esp_log.h"
+#include "esp_ota_ops.h"
 #include "nvs_flash.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include "lwip/netdb.h"
 #include "lwip/api.h"
+
+#include <sys/socket.h>
 
 /* The examples use simple WiFi configuration that you can set via
    'make menuconfig'.
@@ -180,9 +183,122 @@ static void logger_thread(void *p)
 	}
 }
 
+
+static void __attribute__((noreturn)) task_fatal_error()
+{
+	ESP_LOGE(TAG, "Exiting task due to fatal error...");
+	(void)vTaskDelete(NULL);
+
+	while (1) {
+		;
+	}
+}
+
+#define BUFFSIZE 1024
+#define TEXT_BUFFSIZE 1024
+static char text[TEXT_BUFFSIZE];
+static char ota_write_data[BUFFSIZE + 1] = { 0 };
+
+static void ota_thread(void *pvParameter)
+{
+	int binary_file_length = 0;
+	esp_err_t err;
+	esp_ota_handle_t update_handle = 0 ;
+	const esp_partition_t *update_partition = NULL;
+
+	ESP_LOGI(TAG, "Starting OTA example...");
+
+	const esp_partition_t *configured = esp_ota_get_boot_partition();
+	const esp_partition_t *running = esp_ota_get_running_partition();
+
+	if (configured != running) {
+		ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
+			 configured->address, running->address);
+		ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
+	}
+	ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
+		 running->type, running->subtype, running->address);
+
+	struct sockaddr_in sock_info;
+
+	int sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sfd == -1) {
+		ESP_LOGE(TAG, "Create socket failed!");
+		return false;
+	}
+
+	memset(&sock_info, 0, sizeof(struct sockaddr_in));
+	sock_info.sin_family = AF_INET;
+	sock_info.sin_addr.s_addr = htonl(INADDR_ANY);
+	sock_info.sin_port = htons(9999);
+
+	if(bind(sfd, (struct sockaddr *)&sock_info, sizeof(sock_info)) < 0)
+		task_fatal_error();
+
+	if(listen(sfd, 10) < 0)
+		task_fatal_error();
+
+	int socket_id = accept(sfd, NULL, 0);
+
+	send(socket_id, "hello world", 11, 0);
+	update_partition = esp_ota_get_next_update_partition(NULL);
+	ESP_LOGI(TAG, "Writing to partition subtype %d at offset 0x%x",
+		 update_partition->subtype, update_partition->address);
+	assert(update_partition != NULL);
+
+	err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &update_handle);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "esp_ota_begin failed (%s)", esp_err_to_name(err));
+		task_fatal_error();
+	}
+	ESP_LOGI(TAG, "esp_ota_begin succeeded");
+
+	bool flag = true;
+	while (flag) {
+		memset(text, 0, TEXT_BUFFSIZE);
+		memset(ota_write_data, 0, BUFFSIZE);
+		int buff_len = recv(socket_id, text, TEXT_BUFFSIZE, 0);
+		if (buff_len < 0) { /*receive error*/
+			ESP_LOGE(TAG, "Error: receive data error! errno=%d", errno);
+			task_fatal_error();
+		} else if (buff_len > 0) { /*deal with response body*/
+			memcpy(ota_write_data, text, buff_len);
+			err = esp_ota_write( update_handle, (const void *)ota_write_data, buff_len);
+			if (err != ESP_OK) {
+				ESP_LOGE(TAG, "Error: esp_ota_write failed (%s)!", esp_err_to_name(err));
+				task_fatal_error();
+			}
+			binary_file_length += buff_len;
+			ESP_LOGI(TAG, "Have written image length %d", binary_file_length);
+		} else if (buff_len == 0) {  /*packet over*/
+			flag = false;
+			ESP_LOGI(TAG, "Connection closed, all packets received");
+			close(socket_id);
+		} else {
+			ESP_LOGE(TAG, "Unexpected recv result");
+		}
+	}
+
+	ESP_LOGI(TAG, "Total Write binary data length : %d", binary_file_length);
+
+	if (esp_ota_end(update_handle) != ESP_OK) {
+		ESP_LOGE(TAG, "esp_ota_end failed!");
+		task_fatal_error();
+	}
+	err = esp_ota_set_boot_partition(update_partition);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "esp_ota_set_boot_partition failed (%s)!", esp_err_to_name(err));
+		task_fatal_error();
+	}
+	ESP_LOGI(TAG, "Prepare to restart system!");
+	vTaskDelay(5000 / portTICK_RATE_MS);
+	esp_restart();
+	return ;
+}
+
 void app_main_wifi()
 {
-//    wifi_init_softap();
-    wifi_init_sta();
-    xTaskCreate(logger_thread, "logger", 2048, NULL, 5, NULL);
+	wifi_init_sta();
+	xTaskCreate(logger_thread, "logger", 2048, NULL, 5, NULL);
+	xTaskCreate(ota_thread, "ota", 4096, NULL, 5, NULL);
 }
